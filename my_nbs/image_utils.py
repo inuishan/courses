e76640102b2eb14ff71b5381988b8fe03f7ed4a5
ishan <ishanjain1991@gmail.com>
2017-10-01 19:48:44 +0530

    image utils

--- my_nbs/image_utils.py
+++ my_nbs/image_utils.py
@@ -1,9 +1,18 @@
+from os import listdir
+from os.path import isfile, join
+from keras.preprocessing.image import ImageDataGenerator
+from skimage.io import imread_collection
+
+
 def profile_images(dir_path):
     """
     Calculates the profiles of images in a directory
     :param dir_path: The directory path of the images
     :return: ImageProdile containing max_dimensions, min_dimension, avg_dimension
     """
+    onlyfiles = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+    col = imread_collection(dir_path)
+    return col
 
 
 class ImageProfile:
