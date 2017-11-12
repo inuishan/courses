4a512fadaa845f11ec59a0d22b53ccb33a7fe754
ishan <ishanjain1991@gmail.com>
2017-11-12 15:55:56 +0530

    text data prep module

---
+++ dataprep/text_data_prep.py
@@ -0,0 +1,21 @@
+from random import *
+
+
+def prepare_text_data_single_file(main_dir_path, train_path, test_part, clean_dir=False):
+    """
+    This takes single train file & single test file & creates sample files taking a few rows
+    :param main_dir_path: this is the directory where everything should be stored
+    :param train_path: The file name of the training file
+    :param test_part:
+    :param clean_dir:
+    :return:
+    """
+
+
+def random_numbers(upper_range, quantity):
+    """
+    Gives quantity number of random numbers from range 0 to upper range
+    :param upper_range: The upper range of the random numbers, lower is 0
+    :param quantity: The number of random numbers required
+    :return: A list of random numbers
+    """
